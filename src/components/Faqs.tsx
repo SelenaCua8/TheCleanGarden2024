a4a1a3c12ff096790957496a28045350d7ea9c24
= <Selenac99@hotmail.com>
2024-05-20 14:53:21 -0300

    finished footer and faqs

--- src/components/Faqs.tsx
+++ src/components/Faqs.tsx
@@ -6,32 +6,40 @@ import backgroundImage from '@/images/background-faqs.jpg'
 const faqs = [
   [
     {
-      question: 'Does TaxPal handle VAT?',
-      answer:
-        'Well no, but if you move your company offshore you can probably ignore it.',
+      question: '¿Cuáles son los servicios que ofrece su empresa?',
+      answer: 'Nos dedicamos a una amplia rama de servicios de espacios verdes, entre ellos se encuentran: tomografía de árboles; poda, transplante y extracción de ejemplares; diseño y planificación del paisaje; construcción y mantenimiento de fuentes, cascadas y juegos de agua; mantenimiento de espacios verdes; diseño y construcción de sistema de riego; obra pública'
     },
     {
-      question: 'Can I pay for my subscription via purchase order?',
-      answer: 'Absolutely, we are happy to take your money in all forms.',
+      question: '¿Cuál es la ubicación de su empresa?',
+      answer: 'Estamos ubicados en Buenos Aires y en Zárate',
     },
     {
-      question: 'How do I apply for a job at TaxPal?',
+      question: '¿Cuáles son las áreas de servicio específicas de su empresa?',
       answer:
-        'We only hire our customers, so subscribe for a minimum of 6 months and then let’s talk.',
+        'Nuestra empresa brinda servicios de mantenimiento de espacios verdes, construcciones paisajísticas y más en toda la región metropolitana de la capital. Además, gracias a nuestro vivero ubicado en Zarate, tenemos la capacidad de ofrecer servicios especializados y suministro de plantas a clientes en áreas circundantes y más allá. Nos enorgullece extender nuestros servicios a diversas comunidades.',
     },
   ],
   [
     {
-      question: 'What was that testimonial about tax fraud all about?',
+      question: '¿Cuáles son sus métodos y enfoques para el diseño paisajístico?',
       answer:
-        'TaxPal is just a software application, ultimately your books are your responsibility.',
+        'En nuestra empresa, adoptamos un enfoque personalizado para el diseño paisajístico. Nuestro equipo de expertos colabora estrechamente con cada cliente, comprendiendo sus preferencias y visiones. Utilizamos técnicas innovadoras en construcciones paisajísticas, integrando sistemas de riego eficientes y seleccionando cuidadosamente plantas que se adapten al clima local. Nuestra prioridad es transformar espacios verdes en entornos armoniosos y sostenibles que superen las expectativas de nuestros clientes.',
     },
     {
-      question:
-        'TaxPal sounds horrible but why do I still feel compelled to purchase?',
-      answer:
-        'This is the power of excellent visual design. You just can’t resist it, no matter how poorly it actually functions.',
-    },
+      question: 'La forma de trabajo de la empresa ¿en qué se centra?',
+    answer:
+      'La forma de trabajo de nuestra empresa se centra en brindar soluciones integrales en construcción, espacios verdes y paisajismo, respaldadas por un compromiso continuo con el medio ambiente y la satisfacción del cliente. Nuestro enfoque se basa en los siguientes principios:',
+    principles: [
+      'Sostenibilidad Ambiental.',
+      'Diseño Paisajístico.',
+      'Vivero Propio.',
+      'Colaboración con Clientes.',
+      'Calidad y seguridad.',
+      'Innovación y Mejora Continua.'
+    ],
+    summary:
+      'En resumen, nuestra forma de trabajo se basa en la excelencia, la sostenibilidad y la satisfacción del cliente, con un enfoque especial en la creación de entornos que armonicen con la naturaleza y contribuyan positivamente al medio ambiente.'
+  },
     {
       question:
         'I found other companies called TaxPal, are you sure you can use this name?',
@@ -68,42 +76,40 @@ export function Faqs() {
         className="absolute left-1/2 top-0 max-w-none -translate-y-1/4 translate-x-[-30%]"
         src={backgroundImage}
         alt=""
-        width={1558}
-        height={946}
+        
         unoptimized
       />
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2
-            id="faq-title"
-            className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl"
-          >
-            Frequently asked questions
-          </h2>
-          <p className="mt-4 text-lg tracking-tight text-slate-700">
-            If you can’t find what you’re looking for, email our support team
-            and if you’re lucky someone will get back to you.
-          </p>
+        <h2
+  id="faq-title"
+  className="font-display text-3xl tracking-tight text-white sm:text-4xl"
+>
+  Preguntas frecuentes
+</h2>
+<p className="mt-4 text-lg tracking-tight text-white">
+  Si no puede encontrar lo que busca, envíe un correo electrónico a nuestro equipo de soporte y, si tiene suerte, alguien se comunicará con usted.
+</p>
         </div>
         <ul
-          role="list"
-          className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 lg:max-w-none lg:grid-cols-3"
-        >
-          {faqs.map((column, columnIndex) => (
-            <li key={columnIndex}>
-              <ul role="list" className="flex flex-col gap-y-8">
-                {column.map((faq, faqIndex) => (
-                  <li key={faqIndex}>
-                    <h3 className="font-display text-lg leading-7 text-slate-900">
-                      {faq.question}
-                    </h3>
-                    <p className="mt-4 text-sm text-slate-700">{faq.answer}</p>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
+  role="list"
+  className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 lg:max-w-none lg:grid-cols-3"
+>
+  {faqs.map((column, columnIndex) => (
+    <li key={columnIndex}>
+      <ul role="list" className="flex flex-col gap-y-8">
+        {column.map((faq, faqIndex) => (
+          <li key={faqIndex}>
+            <h3 className="font-display text-lg leading-7 text-white">
+              {faq.question}
+            </h3>
+            <p className="mt-4 text-sm text-white">{faq.answer}</p>
+          </li>
+        ))}
+      </ul>
+    </li>
+  ))}
+</ul>
       </Container>
     </section>
   )
