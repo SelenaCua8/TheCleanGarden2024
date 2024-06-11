4fa41c81c58ee61820eca559d99015d01784d380
= <Selenac99@hotmail.com>
2024-06-11 12:33:46 -0300

    finished who are we

--- src/components/SecondaryFeatures.tsx
+++ src/components/SecondaryFeatures.tsx
@@ -6,9 +6,9 @@ import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
-import screenshotContacts from '@/images/screenshots/contacts.png'
-import screenshotInventory from '@/images/screenshots/inventory.png'
-import screenshotProfitLoss from '@/images/screenshots/profit-loss.png'
+import podaDeArboles from '@/images/screenshots/poda-de-arboles.jpg'
+import tomografia from '@/images/screenshots/tomografia.jpg'
+import parqueDeFutbol from '@/images/screenshots/parque-de-futbol.jpg'
 
 interface Feature {
   name: React.ReactNode
@@ -20,11 +20,11 @@ interface Feature {
 
 const features: Array<Feature> = [
   {
-    name: 'Reporting',
-    summary: 'Stay on top of things with always up-to-date reporting features.',
+    name: 'Construcciones de parques',
+    summary: 'Con nuestros profesionales de arquitectura y paisajismo nos dedicamos a la construccion de parques.',
     description:
-      'We talked about reporting in the section above but we needed three items here, so mentioning it one more time for posterity.',
-    image: screenshotProfitLoss,
+      'Nos enorgullece ser lideres de la construcción de parques.',
+    image: parqueDeFutbol, 
     icon: function ReportingIcon() {
       let id = useId()
       return (
@@ -42,68 +42,44 @@ const features: Array<Feature> = [
               <stop offset={1} stopColor="#6692F1" />
             </linearGradient>
           </defs>
-          <path
-            d="m30 15-4 5-4-11-4 18-4-11-4 7-4-5"
-            stroke={`url(#${id})`}
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
+          
         </>
       )
     },
   },
   {
-    name: 'Inventory',
+    name: 'Tomografía de árboles',
     summary:
-      'Never lose track of what’s in stock with accurate inventory tracking.',
+      'Somos la única empresa del país que se dedica al cuidado 100% de los árboles.',
     description:
-      'We don’t offer this as part of our software but that statement is inarguably true. Accurate inventory tracking would help you for sure.',
-    image: screenshotInventory,
+      'Somos una empresa profesional dedicada al cuidado y mantenimiento de árboles de manera nacional.',
+    image: tomografia,
     icon: function InventoryIcon() {
       return (
         <>
-          <path
-            opacity=".5"
-            d="M8 17a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
-            fill="#fff"
-          />
-          <path
-            opacity=".3"
-            d="M8 24a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
-            fill="#fff"
-          />
-          <path
-            d="M8 10a1 1 0 0 1 1-1h18a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-2Z"
-            fill="#fff"
-          />
+          
+         
         </>
       )
     },
   },
   {
-    name: 'Contacts',
+    name: 'Mantenimiento de espacios verdes',
     summary:
-      'Organize all of your contacts, service providers, and invoices in one place.',
+      'Dedicados al mantenimiento de cada espacio verde.',
     description:
       'This also isn’t actually a feature, it’s just some friendly advice. We definitely recommend that you do this, you’ll feel really organized and professional.',
-    image: screenshotContacts,
+    image: podaDeArboles,
     icon: function ContactsIcon() {
       return (
         <>
-          <path
-            opacity=".5"
-            d="M25.778 25.778c.39.39 1.027.393 1.384-.028A11.952 11.952 0 0 0 30 18c0-6.627-5.373-12-12-12S6 11.373 6 18c0 2.954 1.067 5.659 2.838 7.75.357.421.993.419 1.384.028.39-.39.386-1.02.036-1.448A9.959 9.959 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10a9.959 9.959 0 0 1-2.258 6.33c-.35.427-.354 1.058.036 1.448Z"
-            fill="#fff"
-          />
-          <path
-            d="M12 28.395V28a6 6 0 0 1 12 0v.395A11.945 11.945 0 0 1 18 30c-2.186 0-4.235-.584-6-1.605ZM21 16.5c0-1.933-.5-3.5-3-3.5s-3 1.567-3 3.5 1.343 3.5 3 3.5 3-1.567 3-3.5Z"
-            fill="#fff"
-          />
+          
+         
         </>
       )
     },
   },
+  
 ]
 
 function Feature({
@@ -123,7 +99,7 @@ function Feature({
       <div
         className={clsx(
           'w-9 rounded-lg',
-          isActive ? 'bg-blue-600' : 'bg-slate-500',
+          isActive ? 'bg-green-600' : 'bg-green-500',
         )}
       >
         <svg aria-hidden="true" className="h-9 w-9" fill="none">
@@ -133,17 +109,18 @@ function Feature({
       <h3
         className={clsx(
           'mt-6 text-sm font-medium',
-          isActive ? 'text-blue-600' : 'text-slate-600',
+          isActive ? 'text-green-600' : 'text-green-600',
         )}
       >
         {feature.name}
       </h3>
-      <p className="mt-2 font-display text-xl text-slate-900">
+      <p className="mt-2 font-display text-xl text-green-900">
         {feature.summary}
       </p>
-      <p className="mt-4 text-sm text-slate-600">{feature.description}</p>
+      <p className="mt-4 text-sm text-green-600">{feature.description}</p>
     </div>
   )
+  
 }
 
 function FeaturesMobile() {
@@ -237,8 +214,9 @@ export function SecondaryFeatures() {
             ¿Quiénes somos?
           </h2>
           <p className="mt-4 text-lg tracking-tight text-slate-700">
-            Because you’d probably be a little confused if we suggested you
-            complicate your everyday business tasks instead.
+            Somos una empresa con más de 20 años de experiencia orientados a dar soluciones integrales para espacios verdes de mediana y gran escala en espacios públicos, empresas o particulares. <br>
+            </br>
+            Los espacios verdes constituyen uno de los principales articuladores de la vida social. Son lugares de encuentro, de integración, promueven la diversidad cultural generan valor simbólico, identidad y pertenencia. Asimismo, contribuyen en la regulación hídrica y en la reducción del impacto de la ciudad o insdustria construida sobre el medio ambiente. Y ofrecen un ecosistema apropiado para la conservación de la biodiversidad.
           </p>
         </div>
         <FeaturesMobile />
