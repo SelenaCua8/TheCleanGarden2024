4427749b61f3f6a31781c05e36474591eebaa762
= <Selenac99@hotmail.com>
2024-07-23 10:23:10 -0300

    Add Netlify configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Habilitar la exportación estática
-     // Esta opción es necesaria para exportar la aplicación como HTML estático
-    distDir: 'out',   // Directorio de salida para Netlify (debe coincidir con el directorio en el que se genera el build)
-  };
-  
-  module.exports = nextConfig;
+  distDir: 'out', // Directorio de salida para Netlify
+};
+
+module.exports = nextConfig;
