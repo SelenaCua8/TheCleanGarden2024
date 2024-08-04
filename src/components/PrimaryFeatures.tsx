9f6ed70bbca5e08227b27e83f38b4203d5e02b7f
= <Selenac99@hotmail.com>
2024-08-04 15:27:57 -0300

    I just need to add Dark Mode, and then I will finish the page.

--- src/components/PrimaryFeatures.tsx
+++ src/components/PrimaryFeatures.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 import clsx from 'clsx'
 import { Container } from '@/components/Container'
-import backgroundImage from '@/images/background-featuress.jpg'
 import equipamiento from '@/images/screenshots/equipamiento.jpg'
 import flexibilidad from '@/images/screenshots/flexibilidad.jpg'
 import capacitaciones from '@/images/screenshots/capacitaciones.jpg'
@@ -42,12 +41,7 @@ const features = [
       'Inspeccionamos regularmente las obras para garantizar el cumplimiento de los planes de trabajo propuestos, medir la calidad del servicio percibida por el cliente e implementar acciones correctivas, en caso de necesidad.',
     image: control,
   },
-  {
-    title: 'Flexibilidad',
-    description:
-      'Diseñamos planes de trabajos personalizados para dar solución a las necesidades específicas de cada cliente.',
-    image: flexibilidad,
-  },
+  
 ]
 
 export function PrimaryFeatures() {
@@ -72,35 +66,28 @@ export function PrimaryFeatures() {
     <section
       id="features"
       aria-label="Features for running your books"
-      className="relative overflow-hidden bg-blue-600 pb-28 pt-20 sm:py-32"
+      className="relative overflow-hidden bg-green-600 pb-20 pt-12 sm:py-20"
     >
-      <Image
-        className="absolute left-1/2 top-1/2 max-w-none translate-x-[-44%] translate-y-[-42%]"
-        src={backgroundImage}
-        alt=""
-        width={2245}
-        height={1636}
-        unoptimized
-      />
-      <Container className="relative">
+      
+      <Container className="relative px-4 sm:px-6 md:px-8 lg:px-10">
         <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
-          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
+          <h2 className="font-display text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white">
             ¿POR QUÉ ELEGIRNOS?
           </h2>
         </div>
         <TabGroup
-          className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
+          className="mt-8 sm:mt-12 md:mt-16 lg:mt-20 lg:grid lg:grid-cols-12 lg:gap-6"
           vertical={tabOrientation === 'vertical'}
         >
           {({ selectedIndex }) => (
             <>
-              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
-                <TabList className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
+              <div className="flex flex-col gap-y-6 sm:gap-y-8 lg:col-span-5 lg:flex-col lg:gap-y-6">
+                <TabList className="flex flex-col gap-y-6 lg:flex-col lg:gap-y-6">
                   {features.map((feature, featureIndex) => (
                     <div
                       key={feature.title}
                       className={clsx(
-                        'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6',
+                        'group relative rounded-full px-4 py-5 lg:rounded-xl lg:py-10',
                         selectedIndex === featureIndex
                           ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
                           : 'hover:bg-white/10 lg:hover:bg-white/5',
@@ -109,13 +96,13 @@ export function PrimaryFeatures() {
                       <h3>
                         <Tab
                           className={clsx(
-                            'font-display text-lg ui-not-focus-visible:outline-none',
+                            'font-display text-sm sm:text-base lg:text-lg ui-not-focus-visible:outline-none',
                             selectedIndex === featureIndex
-                              ? 'text-blue-600 lg:text-white'
-                              : 'text-blue-100 hover:text-white lg:text-white',
+                              ? 'text-green-900 lg:text-white'
+                              : 'text-green-100 hover:text-white lg:text-white',
                           )}
                         >
-                          <span className="absolute inset-0 rounded-full lg:rounded-l-xl lg:rounded-r-none" />
+                          <span className="absolute inset-0 rounded-full lg:rounded-xl" />
                           {feature.title}
                         </Tab>
                       </h3>
@@ -124,7 +111,7 @@ export function PrimaryFeatures() {
                           'mt-2 hidden text-sm lg:block',
                           selectedIndex === featureIndex
                             ? 'text-white'
-                            : 'text-blue-100 group-hover:text-white',
+                            : 'text-green-100 group-hover:text-white',
                         )}
                       >
                         {feature.description}
@@ -137,12 +124,12 @@ export function PrimaryFeatures() {
                 {features.map((feature) => (
                   <TabPanel key={feature.title} unmount={false}>
                     <div className="relative sm:px-6 lg:hidden">
-                      <div className="absolute -inset-x-4 bottom-[-4.25rem] top-[-6.5rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
+                      <div className="absolute inset-x-0 bottom-[-4rem] top-[-1rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
                       <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
                         {feature.description}
                       </p>
                     </div>
-                    <div className="mt-10 max-w-full overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-blue-900/20">
+                    <div className="mt-6 sm:mt-8 lg:mt-10 max-w-full overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-green-900/20">
                       <Image
                         className="w-full h-auto object-cover"
                         src={feature.image}
