2a2c93a90a370b1ef1d58f264fc255546dd7eb9c
= <Selenac99@hotmail.com>
2024-09-09 11:47:31 -0300

    again

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ import { Container } from '@/components/Container'
 export function Hero() {
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
-      <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-green-900 sm:text-7xl" dark:text-white>
+      <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-green-900 sm:text-7xl dark:text-white">
         Somos{' '}
         <span className="relative whitespace-nowrap text-green-600">
           <svg
